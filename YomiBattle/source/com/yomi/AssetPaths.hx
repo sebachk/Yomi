94f08f2d5112b301af20aadc884d73115c6c740f
sebachk <sebachk90@gmail.com>
2016-06-28 15:25:36 -0300

    Commit inicial nuevo Proyecto

---
+++ YomiBattle/source/com/yomi/AssetPaths.hx
@@ -0,0 +1,4 @@
+package com.yomi;
+
+@:build(flixel.system.FlxAssets.buildFileReferences("assets", true))
+class AssetPaths {}
---
+++ YomiBattle/source/com/yomi/Main.hx
@@ -0,0 +1,13 @@
+package com.yomi;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(640, 480, MenuState));
+	}
+}
